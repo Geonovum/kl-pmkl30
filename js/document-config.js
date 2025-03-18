f43051b3449af0bddcb3da6054387da94f52e4b8
GN-BOT-WORKFLOW <beheer@geonovum.nl>
2025-03-18 10:54:43 +0000

    automated commit

---
+++ js/document-config.js
@@ -0,0 +1,46 @@
+var documentConfig =
+{
+  title: "Handreiking Visualisatie (PMKL) - versie 3.0",
+  shortName: "pmkl30",
+  pubDomain: "kl",
+  specStatus: "def",
+  specType: "st",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/kl/pmkl30/"
+  ],
+  publishDate: "2025-03-18",
+  publishVersion: "3.0.0",
+  thisVersion: [
+    "https://docs.geostandaarden.nl/kl/def-st-pmkl30-20250318/"
+  ],
+  previousPublishDate: "2024-09-30",
+  previousPublishVersion: "3.0.0",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/kl/cv-st-pmkl30-20240930/"
+  ],
+  previousMaturity: "cv",
+  github: "https://github.com/geonovum/kl-pmkl30/",
+  issueBase: "https://github.com/geonovum/kl-pmkl30/issues/",
+  edDraftURI: "https://geonovum.github.io/kl-pmkl30/",
+  canonicalURI: "https://docs.geostandaarden.nl/kl/pmkl30/",
+  authors: [
+    {
+      name: "Edward Mac Gillavry",
+      company: "Webmapper",
+      companyURL: "https://webmapper.net/"
+    }
+  ],
+  editors: [
+    {
+      name: "Edward Mac Gillavry",
+      company: "Webmapper",
+      companyURL: "https://webmapper.net/"
+    },
+    {
+      name: "Paul Janssen",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ]
+}
